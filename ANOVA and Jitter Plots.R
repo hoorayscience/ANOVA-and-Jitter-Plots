2952c6540adea8b4f73358f38e99b96c36b560a7
hoorayscience <elliew@sccwrp.org>
2020-12-15 16:05:14 -0800

    Data with no spike

--- ANOVA and Jitter Plots.R
+++ ANOVA and Jitter Plots.R
@@ -21,6 +21,8 @@ library(viridis)
 Anti.AR.Data <- read_csv("Anti-AR.Strata.csv")
 head(Anti.AR.Data)
 
+Anti.AR.Data.No.Spike <- read_csv("Anti-AR.Strata.No.Spike.csv")
+head(Anti.AR.Data)
 #boxplot with jitter AhR
 
 Anti.AR.Data %>%
@@ -36,5 +38,17 @@ Anti.AR.Data %>%
   theme(axis.title.y=element_text(size=16, vjust=5))+
   theme(plot.margin = unit(c(1,1,1,1), "cm"))
 
+Anti.AR.Data.No.Spike %>%
+  ggplot(aes(x=reorder(Stratum,inhibition,na.rm = TRUE), y=inhibition, fill=Stratum)) +
+  geom_boxplot() +
+  geom_jitter(color="black", size=1.5, alpha=0.9, width = 0.1) +
+  theme_classic() +
+  theme(legend.position="none", plot.title = element_text(size=18)) +
+  ylim(0,40)+
+  ylab("% inhibition relative to Cyproterone acetate") +
+  theme(axis.title.x=element_blank())+
+  theme(axis.text=element_text(size=16))+
+  theme(axis.title.y=element_text(size=16, vjust=5))+
+  theme(plot.margin = unit(c(1,1,1,1), "cm"))
 
 
